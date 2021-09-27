29c8137f788a14146a948a8c3262bea00809ff8e
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-26 21:35:22 -0300

    Adicionar arquivos de projeto.

---
+++ StarWarsSpaceShipManager/ConnectionDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace StarWarsSpaceShipManager
+{
+    class ConnectionDB
+    {
+        //instância à classe SqlConnection
+        SqlConnection connection = new SqlConnection();
+
+        //Método construtor
+        public ConnectionDB()
+        {
+            connection.ConnectionString = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+        }
+
+
+        //método de conexão
+        public SqlConnection connect()
+        {
+            //verifica se a conexão ainda não está aberta
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                this.connection.Open();
+            }
+
+            return this.connection;
+        }
+
+
+        //método de desconexão
+        public void disconnect()
+        {
+            if(connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+
+        }
+
+    } 
+}
