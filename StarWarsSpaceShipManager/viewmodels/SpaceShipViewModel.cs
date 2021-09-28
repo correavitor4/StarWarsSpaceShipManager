2e303c519a8d44d42ac58de2d83a34b45ab52d63
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-27 22:50:35 -0300

    Criação da classe de viewModel de naves

---
+++ StarWarsSpaceShipManager/viewmodels/SpaceShipViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWarsSpaceShipManager.viewmodels
+{
+    class SpaceShipViewModel
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Passengers { get; set; }
+        public string Cargo_Capacity { get; set; }
+        public string Starship_Class { get; set; }
+        public string Url { get; set; }
+
+    }
+}
