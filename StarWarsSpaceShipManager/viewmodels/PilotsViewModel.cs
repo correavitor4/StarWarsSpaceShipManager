c487a30a43f8d0b2dfaf66d48991420fed0b54ef
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-29 11:51:59 -0300

    Inseri o shipsId na classe

---
+++ StarWarsSpaceShipManager/viewmodels/PilotsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWarsSpaceShipManager.viewmodels
+{
+    class PilotsViewModel
+    {
+        public string Name { get; set; }
+        public string Birth_Year { get; set; }
+        public string Homeworld { get; set; }
+        public List<string> Starships { get; set; }
+        public string Url { get; set; }
+
+        public List<int> ShipsId = new List<int>();
+    }
+}
