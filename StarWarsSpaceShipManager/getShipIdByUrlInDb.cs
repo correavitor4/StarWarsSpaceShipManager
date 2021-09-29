12e016b45d4bf9b2a432eb1beaa912e203ec12bc
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-29 11:52:12 -0300

    Retorne o Id da nave com base na URL

---
+++ StarWarsSpaceShipManager/getShipIdByUrlInDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace StarWarsSpaceShipManager
+{
+    class getShipIdByUrlInDb
+    {
+        ConnectionDB conn = new ConnectionDB();
+        SqlCommand cmd = new SqlCommand();
+        public string result;
+
+        public getShipIdByUrlInDb(string url)
+        {
+            try
+            {
+                
+
+
+
+                cmd.Connection= conn.connect();
+
+                cmd.CommandText = string.Format("SELECT [IdNave] FROM [dbo].[Naves] WHERE [Url]='{0}'", url);
+
+                SqlDataReader reader =cmd.ExecuteReader();
+
+
+
+                while (reader.Read())
+                {
+                    result = reader["IdNave"].ToString();
+                }
+
+                
+
+                conn.disconnect();
+
+
+                
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+        }
+    }
+}
